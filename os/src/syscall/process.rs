57cba829c0d7c18203d83f9ee845bf71cd21a80a
DaZhou-ECHO <2821511460@qq.com>
2024-11-04 17:11:09 +0800

    11/4/17

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -114,26 +114,77 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
     // ---- release current PCB automatically
 }
 
+
+use crate::mm::translated_byte_buffer;
+use crate::timer::get_time_us;
+
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    // trace!(
+    //     "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
+    //     current_task().unwrap().pid.0
+    // );
+    // -1
+    let us = get_time_us();
+    let dst_vec = translated_byte_buffer(
+    current_user_token(),
+    ts as *const u8,
+    core::mem::size_of::<TimeVal>()
     );
-    -1
+
+    let ref time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+
+    let src_ptr = time_val as *const TimeVal;
+
+    for (idx, dst) in dst_vec.into_iter().enumerate() {
+        let unit_len = dst.len();
+        unsafe {
+            dst.copy_from_slice(core::slice::from_raw_parts(
+                src_ptr.wrapping_byte_add(idx * unit_len) as *const u8,
+                unit_len
+        ));
+    }
+}
+
+0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TaskInfo`] is splitted by two pages ?
-pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_task_info NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
+    // trace!(
+    //     "kernel:pid[{}] sys_task_info NOT IMPLEMENTED",
+    //     current_task().unwrap().pid.0
+    // );
+    // -1
+    let dst_vec = translated_byte_buffer(
+        current_user_token(),
+        ti as *const u8,
+        core::mem::size_of::<TimeVal>()
+        );
+    
+    let ref task = TaskInfo{
+        status: get_current_task_status(),
+        syscall_times: get_current_task_syscall_times(),
+        time: get_current_task_time_cost(),
+    };
+    let src_ptr = task as *const TaskInfo;
+    for (idx, dst) in dst_vec.into_iter().enumerate() {
+        let unit_len = dst.len();
+        unsafe {
+            dst.copy_from_slice(core::slice::from_raw_parts(
+                src_ptr.wrapping_byte_add(idx * unit_len) as *const u8,
+                unit_len
+            ));
+            }
+    }
+    0
 }
 
 /// YOUR JOB: Implement mmap.
